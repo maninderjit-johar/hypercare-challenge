cf198297a35f1fac612231087ea409d7e976014d
Maninderjit Johar <mani@gomarketbox.com>
2024-05-02 08:42:48 -0400

    just oone quck test within the time limit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { UserCard } from "./components/UserCard";
+import { UserList } from "./components/UserList";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders user card with correct user information", () => {
+  const user = {
+    avatar: "",
+    firstname: "John",
+    lastname: "Doe",
+    description: "Checking",
+    role: "Developer",
+    join_date: "5/21/2024",
+    id: "1",
+  };
+
+  render(<UserCard user={user} onViewMore={() => console.log(user)} />);
+
+  // Check if user information is rendered correctly
+  expect(
+    screen.getByText(`${user.firstname} ${user.lastname}`)
+  ).toBeInTheDocument();
+  expect(screen.getByText(user.description)).toBeInTheDocument();
+  expect(
+    screen.getByAltText(`${user.firstname} ${user.lastname}`)
+  ).toBeInTheDocument();
 });
